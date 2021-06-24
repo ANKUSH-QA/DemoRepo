c38b4eb11e126ae027f78334a0cd6a1c3059cb7d
Ankush Singhal <ankush.sighal@aptushealth.com>
2021-06-24 19:18:15 +0530

    added new  line

--- src/test/java/com/qa/tests/Test1.java
+++ src/test/java/com/qa/tests/Test1.java
@@ -5,6 +5,13 @@ public class Test1 {
 	
 	public void logintest() {
 		System.out.println("Test Passed");
+		
+		System.out.println("Test Passed");
+		System.out.println("Test Passed");
+		
+		System.out.println("Test Passed");
+		
+		System.out.println("Test Passed");
 	}
 	
 }
